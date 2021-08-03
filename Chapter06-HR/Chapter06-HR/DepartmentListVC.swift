08f6d7a1ec3f7e7332d632a32bf6ac18f26ca1a9
bsh0137 <bookert0137@naver.com>
2021-08-03 21:46:09 +0900

    update src

---
+++ Chapter06-HR/Chapter06-HR/DepartmentListVC.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class DepartmentListVC : UITableViewController {
+    
+}
---
+++ Chapter06-HR/Chapter06-HR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter06-HR
+//
+//  Created by 백성현 on 2021/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
