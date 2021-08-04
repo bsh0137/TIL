e9c7a30b066263597c348f4f26c8d6577a6051f9
bsh0137 <bookert0137@naver.com>
2021-08-04 21:33:42 +0900

    update src

--- Chapter06-HR02/Chapter06-HR02/DepartmentDAO.swift
+++ Chapter06-HR02/Chapter06-HR02/DepartmentDAO.swift
@@ -1,4 +1,5 @@
 class DepartmentDAO {
+    // 부서 정보를 담을 튜플 타입 정의
     typealias DepartRecord = (Int, String, String)
     
     // SQLite 연결 및 초기화
@@ -6,12 +7,11 @@ class DepartmentDAO {
         // 1. 파일 매니저 객체를 생성
         let fileMgr = FileManager.default
         
-        // 2. 샌드박스 내 문서 디렉토리에서 데이터베이스 파일 경로를 확인
+        // 2. 샌드박스 내 문서 디렉터리에서 데이트베이스 파일 경로 확인
         let docPath = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first
         let dbPath = docPath!.appendingPathComponent("hr.sqlite").path
-        print("path: \(dbPath)")
         
-        // 3. 샌드박스 경로에 파일이 없다면 메인 번들에 만들어 둔 hr.sqlite를 가져와 복사
+        // 3. 샌드박스 경로에 파일이 없다면 메인 번들에 만들어 둔 hr.sqlite를 가져와 복사.
         if fileMgr.fileExists(atPath: dbPath) == false {
             let dbSource = Bundle.main.path(forResource: "hr", ofType: "sqlite")
             try! fileMgr.copyItem(atPath: dbSource!, toPath: dbPath)
@@ -29,7 +29,7 @@ class DepartmentDAO {
         self.fmdb.close()
     }
     
-    func find() -> [DepartRecord] {
+    func find () -> [DepartRecord] {
         // 반환할 데이터를 담을 [DepartRecord] 타입의 객체 정의
         var departList = [DepartRecord]()
         
@@ -40,6 +40,7 @@ class DepartmentDAO {
                 FROM department
                 ORDER BY depart_cd ASC
                 """
+            
             let rs = try self.fmdb.executeQuery(sql, values: nil)
             
             // 2. 결과 집합 추출
@@ -49,7 +50,7 @@ class DepartmentDAO {
                 let departAddr = rs.string(forColumn: "depart_addr")
                 
                 // append 메소드 호출 시 아래 튜플을 괄호 없이 사용하지 않도록 주의
-                departList.append( ( Int(departCd), departTitle!, departAddr! ))
+                departList.append( ( Int(departCd), departTitle!, departAddr! ) )
             }
         } catch let error as NSError {
             print("failed: \(error.localizedDescription)")
@@ -68,7 +69,7 @@ class DepartmentDAO {
         let rs = self.fmdb.executeQuery(sql, withArgumentsIn: [departCd])
         
         // 결과 집합 처리
-        if let _rs = rs { // 결과 집합이 옵셔널 타입으로 반환되므로, 이를 일반 상수에 바인딩하여 해제한다.
+        if let _rs = rs {
             _rs.next()
             
             let departId = _rs.int(forColumn: "depart_cd")
@@ -76,44 +77,18 @@ class DepartmentDAO {
             let departAddr = _rs.string(forColumn: "depart_addr")
             
             return (Int(departId), departTitle!, departAddr!)
-        } else {
+        } else { // 결과 집합이 없을 경우 nil을 반환한다.
             return nil
         }
     }
     
-    func create(title: String!, addr: String!) -> Bool {
-        // 인자 값이 nil 혹은 공백 문자인지 확인.
-        guard title != nil && title?.isEmpty == false else {
-            return false
-        }
-        
-        guard addr != nil && addr?.isEmpty == false else {
-            return false
-        }
-        
+    func create(Title: String!, Addr: String!) -> Bool {
         do {
             let sql = """
                 INSERT INTO department (depart_title, depart_addr)
                 VALUES ( ? , ? )
                 """
-            try self.fmdb.executeUpdate(sql, values: [title!, addr!])
-            return true
-        } catch let error as NSError {
-            print("Insert Error : \(error.localizedDescription)")
-            return false
+            try self.fmdb.executeUpdate(<#T##sql: String##String#>, values: <#T##[Any]?#>)
         }
-    }
-
-    // 부서 정보를 삭제함
-    func remove(departCd: Int) -> Bool {
-        do {
-            let sql = "DELETE FROM department WHERE depart_cd = ? "
-            try self.fmdb.executeQuery(sql, values: [departCd])
-            return true
-        } catch let error as NSError {
-            print("DELETE Error : \(error.localizedDescription)")
-            return false
-        }
-        
     }
 }
---
+++ Chapter06-HR02/Chapter06-HR02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter06-HR02
+//
+//  Created by 백성현 on 2021/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
